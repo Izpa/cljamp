310e9722dc53797376cbbf659b9e608d90b24c88
izpa <ispius.prime@gmail.com>
2023-12-27 06:58:54 +0300

    add http-kit

--- src/core.clj
+++ src/core.clj
@@ -1,9 +1,13 @@
-(ns core)
+(ns core
+  (:require [org.httpkit.server :as hk-server]))
 
 (defn hello-world
   []
   "Hello world!")
 
-(defn main
-  []
-  (print (hello-world)))
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
+(def my-server (hk-server/run-server app {:port 8888}))
